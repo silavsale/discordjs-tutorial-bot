c99a873eb31f10afeaa2334f8720a0579464953c
Tiger <silavsale@gmail.com>
2021-07-19 09:33:49 -0400

    added reply to greeting

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,22 @@
 require("dotenv").config()
-
 const { Client } = require("discord.js")
 const client = new Client()
 
-client.login(process.env.DISCORDJS_BOT_TOCKEN)
+client.on("ready", () => {
+  console.log(`${client.user.username} has logged in.`)
+})
 
-console.log("BOT is running!")
+client.on("message", (message: any) => {
+  if (message.author.bot) return
+  let messageText = message.content
+  messageText = messageText.toLowerCase().trim()
+
+  if (messageText.includes("hi") || messageText.includes("hello")) {
+    // message.channel.send(
+    //   `Hello [${message.author.tag}]  and welcome to DevBandaUa`
+    // )
+    message.reply(`Hello and welcome to DevBandaUa`)
+  }
+})
+
+client.login(process.env.DISCORDJS_BOT_TOCKEN)
